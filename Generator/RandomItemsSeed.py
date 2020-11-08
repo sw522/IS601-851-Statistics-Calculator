5d4680802c96b305062d31795c75fc22a872f491
sw522 <71283535+sw522@users.noreply.github.com>
2020-11-08 12:30:10 -0500

    Feature: Added Random Items With Seed function

---
+++ Generator/RandomItemsSeed.py
@@ -0,0 +1,8 @@
+import random
+from operator import itemgetter
+from Generator.RandomNumber import randomnumber
+from Generator.RandomListSeed import randomlistseed
+
+def randomitemsseed(a, n, seed):
+    b = randomlistseed(0, len(a), n, seed)
+    return list(itemgetter(*b)(a))
