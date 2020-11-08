d3850408ad311f065382500b34637770ee1ca681
sw522 <71283535+sw522@users.noreply.github.com>
2020-11-08 12:36:17 -0500

    Feature: Added Sampling Class

---
+++ Sampling/Sampling.py
@@ -0,0 +1,39 @@
+from Generator.Generator import Generator
+from Sampling.SimpleRandomSampling import simplerandomsampling
+from Sampling.SimpleRandomSamplingSeed import simplerandomsamplingseed
+from Sampling.ConfidenceInterval import confidenceinterval
+from Sampling.MarginOfError import marginoferror
+from Sampling.Cochrans import cochrans
+from Sampling.FindSampleSize import findsamplesize
+import pprint
+
+class Sampling(Generator):
+
+    data = []
+
+    def __init__(self):
+        super().__init__()
+
+    def srs(self, data, n):
+        self.result = simplerandomsampling(data, n)
+        return self.result
+
+    def srsseed(self, data, n, seed):
+        self.result = simplerandomsamplingseed(data, n, seed)
+        return self.result
+
+    def moe(self, data, conf):
+        self.result = marginoferror(data, conf)
+        return self.result
+
+    def conf_int(self, data, conf):
+        self.result = confidenceinterval(data, conf)
+        return self.result
+
+    def cochrans_n(self, data, conf, prop):
+        self.result = cochrans(data, conf, prop)
+        return self.result
+
+    def find_n(self, conf, width):
+        self.result = findsamplesize(conf, width)
+        return self.result
