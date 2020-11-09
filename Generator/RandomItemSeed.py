5483fb6618f54c2365c8e1f3d96727d00449219f
sw522 <71283535+sw522@users.noreply.github.com>
2020-11-08 22:56:51 -0500

    Fix: Again addressed a bug where the input should have been len(a) - 1 instead of len(a)

--- Generator/RandomItemSeed.py
+++ Generator/RandomItemSeed.py
@@ -3,5 +3,5 @@ from Generator.RandomNumber import randomnumber
 
 def randitemseed(a, seed):
     random.seed(seed)
-    b = randomnumber(0, len(a))
+    b = randomnumber(0, len(a) - 1)
     return a[b]
--- Generator/RandomItemsSeed.py
+++ Generator/RandomItemsSeed.py
@@ -4,5 +4,5 @@ from Generator.RandomNumber import randomnumber
 from Generator.RandomListSeed import randomlistseed
 
 def randomitemsseed(a, n, seed):
-    b = randomlistseed(0, len(a), n, seed)
+    b = randomlistseed(0, len(a) -1 , n, seed)
     return list(itemgetter(*b)(a))
