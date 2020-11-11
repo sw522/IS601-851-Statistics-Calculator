Fix: Fixed Cochrans formula and updated unit tests to use csv files

Sampling/Cochrans.py
@@ -1,16 +1,16 @@
-from Sampling.MarginOfError import marginoferror
-from Calculator.SquareRoot import squarerooting
+from Calculator.Square import squaring
 import scipy.stats
 from math import ceil
 
-def cochrans(a, conf, prop):
+def cochrans(conf, prop):
 
     z_critical = z_critical = scipy.stats.norm.ppf(1 - (1 - conf) / 2)
-    z_critical_squared = squarerooting(z_critical)
+    z_critical_squared = squaring(z_critical)
 
-    moe = marginoferror(a,conf)
-    moe_squared = squarerooting(moe)
-    cochrans_n = ceil((z_critical_squared * prop * (1-prop)) / moe_squared)
+    e = (1-conf)
+    e_squared = squaring(e)
+    cochrans_n = ceil((z_critical_squared * prop * (1-prop)) / e_squared)
+
     return cochrans_n
 
 

Tests/test_Sampling.py
@@ -3,41 +3,59 @@ from Sampling.Sampling import Sampling
 import pprint
 import scipy.stats
 import numpy as np
+import random
+from CsvReader.CsvReader import CsvReader
 
 class MyTestCase2(unittest.TestCase):
     def setUp(self) -> None:
         #random.seed(5)
-        self.testData = [0,1,2,3,4,5,6,6]
-        self.n = 3
+        self.testData = []
+        dataset_reader = CsvReader('Tests/Dataset.csv').data
+        for row in dataset_reader:
+            self.testData.append(int(row['DataSet']))
+
+        self.srs = []
+
+        srs_reader = CsvReader('Tests/UnitTestSrsAnswers.csv').data
+        for row in srs_reader:
+            self.srs.append(int(row['srs']))
+
+        stat_answers = CsvReader('Tests/UnitTestSamplingInputs.csv').data
+        for row in stat_answers:
+            self.n = int(row['n'])
+            self.seed = int(row['seed'])
+            self.conf = float(row['conf'])
+            self.prop = float(row['prop'])
+            self.width = float(row['width'])
+
+        stat_answers = CsvReader('Tests/UnitTestSamplingAnswers.csv').data
+        for row in stat_answers:
+            self.mean = float(row['mean'])
+            self.moe = float(row['moe'])
+            self.cochrans_n = float(row['cochrans'])
+            self.sample_n = float(row['find_n'])
+
         self.sampling = Sampling()
-        self.seed = 0
-        self.conf = .95
-        self.prop = .5
-        self.width = .06
 
     def test_instantiate_sampling(self):
         self.assertIsInstance(self.sampling, Sampling)
 
     def test_srs_sampling(self):
         srs = self.sampling.srsseed(self.testData, self.n, self.seed)
-        #pprint.pprint(srs)
-        self.assertEqual(srs, [6, 3, 5])
+        self.assertEqual(srs, self.srs)
 
     def test_moe_sampling(self):
         moe = self.sampling.moe(self.testData, self.conf)
-        self.assertEqual(round(moe,3), round(scipy.stats.stats.sem(self.testData) * 1.96,3))
+        self.assertEqual(round(moe,3), self.moe)
 
     def test_ci_sampling(self):
         ci = self.sampling.conf_int(self.testData, self.conf)
-        np_mean = np.mean(self.testData)
-        scipy_sem = scipy.stats.stats.sem(self.testData)
-        np_ci = (round(np_mean,3),round(np_mean-(1.96 * scipy_sem),3),round(np_mean+(1.96 * scipy_sem),3))
-        self.assertEqual(ci, np_ci)
+        self.assertEqual(ci, (self.mean, self.mean - self.moe, round(self.mean + self.moe,2) ))
 
     def test_cochrans_sampling(self):
-        cochrans_n = self.sampling.cochrans_n(self.testData, self.conf, self.prop)
-        self.assertEqual(cochrans_n, 1)
+        cochrans_n = self.sampling.cochrans_n(self.conf, self.prop)
+        self.assertEqual(cochrans_n, self.cochrans_n)
 
     def test_find_n_sampling(self):
         sample_n = self.sampling.find_n(self.conf,self.width)
-        self.assertEqual(1, 1)
+        self.assertEqual(sample_n, self.sample_n)

Tests/test_Statistics.py
@@ -4,49 +4,62 @@ from Statistics.Statistics import Statistics
 import pprint
 import numpy as np
 import scipy.stats
+from CsvReader.CsvReader import CsvReader
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         random.seed(5)
-        self.testData = [0,1,2,3,4,5,6,6] #randint(0, 10, 20)
+        #self.testData = [0,1,2,3,4,5,6,6] #randint(0, 10, 20)
 
         self.statistics = Statistics()
+        self.testData = []
+        self.zscoredata = []
+
+        dataset_reader = CsvReader('Tests/Dataset.csv').data
+        for row in dataset_reader:
+            self.testData.append(int(row['DataSet']))
+
+        stat_answers = CsvReader('Tests/UnitTestStatAnswers.csv').data
+        for row in stat_answers:
+            self.mean = float(row['mean'])
+            self.median = float(row['median'])
+            self.mode = float(row['mode'])
+            self.variance = float(row['variance'])
+            self.stddev = float(row['stddev'])
+
+        zscore_reader = CsvReader('Tests/UnitTestZScoreAnswers.csv').data
+        for row in zscore_reader:
+            self.zscoredata.append(float(row['zscore']))
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.statistics, Statistics)
 
     def test_mean_calculator(self):
-        np_mean = round(np.mean(self.testData), 9)
         mean = self.statistics.mean(self.testData)
-        self.assertEqual(mean, np_mean)
+
+        self.assertEqual(mean, self.mean)
 
     def test_median_calculator(self):
-        np_median = round(np.median(self.testData), 9)
         median = self.statistics.median(self.testData)
-        self.assertEqual(median, 3.5)
+        self.assertEqual(median, self.median)
 
     def test_mode_calculator(self):
-        np_mode = scipy.stats.mode(self.testData)
-        np_mode_value = np_mode[0][0]
         mode = self.statistics.mode(self.testData)
-        self.assertEqual(mode, np_mode_value)
+        self.assertEqual(mode, self.mode)
 
     def test_stddev_calculator(self):
-        np_stddev = round(np.std(np.array(self.testData)), 5)
         stddev = self.statistics.stddev(self.testData)
-        self.assertEqual(stddev, np_stddev)
+        self.assertEqual(stddev, self.stddev)
 
     def test_variance_calculator(self):
-        np_variance = round(np.var(np.array(self.testData)), 9)
         variance = self.statistics.variance(self.testData)
-        self.assertEqual(variance, np_variance)
+        self.assertEqual(variance, self.variance)
 
     def test_zscore_calculator(self):
-        np_zscore = scipy.stats.zscore(self.testData)
         ZScore = self.statistics.zscore(self.testData)
 
-        for i in range(0,len(np_zscore)-1):
-            self.assertEqual(round(ZScore[i],4), round(np_zscore[i],4))
+        for i in range(0,len(ZScore)-1):
+            self.assertEqual(round(ZScore[i],4), round(self.zscoredata[i],4))
 
 
 if __name__ == '__main__':